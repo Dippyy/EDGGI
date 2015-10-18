34a608a3b7421ec8d4865d7c540a9efa237ff4e5
Robert <robert_dipp@hotmail.com>
2015-10-18 16:17:53 -0400

    Checking the Errors

--- SGTitleScreenScene.h
+++ SGTitleScreenScene.h
@@ -3,7 +3,7 @@
 //  SlyGuy
 //
 //  Created by Robert D'Ippolito on 2014-09-12.
-//  Copyright (c) 2014 Robert D'Ippolito. All rights reserved.
+//  Copyright (c) 2014 Robert D'Ippolito. All rights reserved. Checking to see if this is working
 //
 
 #import <SpriteKit/SpriteKit.h>
