30db52486af86fd12963acdf8b6ca09c783c513b
Alex <alexzhou@outlook.com>
2015-12-05 14:58:23 -0500

    Testing a second merge -Alex

--- UTIL.h
+++ UTIL.h
@@ -4,7 +4,7 @@
 //
 //  Created by Robert D'Ippolito on 2014-09-14.
 //  Copyright (c) 2014 Robert D'Ippolito. All rights reserved.
-//
+//  Alex changes something again 
 
 #import <Foundation/Foundation.h>
 
