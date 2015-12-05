06268315c7ae2cfe848deedab82803a2e2dc61b7
Robert <robert_dipp@hotmail.com>
2015-12-05 17:09:46 -0500

    Latest-Rob

--- UTIL.h
+++ UTIL.h
@@ -4,10 +4,6 @@
 //
 //  Created by Robert D'Ippolito on 2014-09-14.
 //  Copyright (c) 2014 Robert D'Ippolito. All rights reserved.
-//  THIS IS TESTING GITHUB COLLABORATION_1
-//  Alex changes something again
-//  Rob has made a second change
-//  Second Test -Alex
 
 
 #import <Foundation/Foundation.h>
