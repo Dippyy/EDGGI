02b3cabf3b7d0841b0e24539c60a071ad5df828a
Alex <alexzhou@outlook.com>
2015-12-05 15:12:02 -0500

    Pushing to remote 1.0 -Alex

--- UTIL.h
+++ UTIL.h
@@ -4,6 +4,7 @@
 //
 //  Created by Robert D'Ippolito on 2014-09-14.
 //  Copyright (c) 2014 Robert D'Ippolito. All rights reserved.
+//  THIS IS TESTING GITHUB COLLABORATION_1
 //  Alex changes something again
 
 #import <Foundation/Foundation.h>
